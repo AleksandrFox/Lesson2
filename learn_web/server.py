b517a7be46b0aa3b4726ddf0bdbdf35e38dc5b42
Ams <sasha_ams@mail.ru>
2022-12-26 08:44:56 +0500

    'погода'

--- learn_web/server.py
+++ learn_web/server.py
@@ -1,10 +1,16 @@
+from distutils.log import debug
 from flask import Flask
+from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return 'Привет, Серёжа!'
+def index():
+    weather = weather_by_city('Ekaterinburg,Russia')
+    if weather:
+        return f"<h2>Погода: {weather['temp_C']}, ощущается как:  {weather['FeelsLikeC']}</h2>"
+    else:
+        return 'Сервис погоды временно недоступен :('
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) 
