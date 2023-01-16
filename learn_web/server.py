806a383985eb5847cc8d9d4b0da23e78718e1e2d
Ams <sasha_ams@mail.ru>
2023-01-16 06:17:03 +0500

    сервер с html

--- learn_web/server.py
+++ learn_web/server.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    page_title = 'Прогноз погоды'
-    weather = weather_by_city('Ekaterinburg,Russia')
-    if weather:
-        weather_text = f"Погода: {weather['temp_C']}, ощущается как:  {weather['FeelsLikeC']}"
-    else:
-        weather_text = 'Сервис погоды временно недоступен :('
-    return render_template("index.html", page_title=page_title, weather_text=weather_text)
+    title = 'Прогноз погоды'
+    weather = weather_by_city('Ekaterinburg,Russia')    
+    return render_template("index.html", page_title=title, weather=weather)
 
 if __name__ == '__main__':
     app.run(debug=True) 
