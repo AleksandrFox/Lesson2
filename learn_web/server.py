16f598e9a27d15be23f9695d7de0bda095577dd9
Ams <sasha_ams@mail.ru>
2023-01-16 07:57:10 +0500

    берем новости с пайтон орг

--- learn_web/server.py
+++ learn_web/server.py
@@ -1,5 +1,7 @@
 from distutils.log import debug
 from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -7,8 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = 'Новости Python'
-    weather = weather_by_city('Ekaterinburg,Russia')    
-    return render_template("index.html", page_title=title, weather=weather)
+    weather = weather_by_city('Ekaterinburg,Russia')
+    news_list = get_python_news()
+    return render_template("index.html", page_title=title, weather=weather, news_list=news_list)
 
 if __name__ == '__main__':
     app.run(debug=True) 
